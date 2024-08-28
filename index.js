fafdcbae902e68500cbd6d327969ca111ff094b1
BurcuMengu <burcumengu@gmail.com>
2024-08-28 18:45:18 +0300

    push

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
-import dotenv from "dotenv";
-dotenv.config();
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
+env.config();
 
-const db_password = process.env.DB_PASSWORD;
 
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "world",
-    password: db_password,
-    port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.PG_PORT,
 });
 
 db.connect();
